33cb62f00946479a7cd6de990385fff2fe49a65c
airtonbjunior <airtonbjunior@gmail.com>
2019-01-09 08:00:01 -0200

    gitignore updated, aux and assign modified

--- deep-learning/assignment-1/assign1.py
+++ deep-learning/assignment-1/assign1.py
@@ -18,7 +18,9 @@ from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
 
-from aux import *
+from aux import * # aux methods, as can be seen at https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/udacity/1_notmnist.ipynb
 
 if __name__ == '__main__':
+	train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+	test_filename  = maybe_download('notMNIST_small.tar.gz', 8458043)
 	print("hey")
